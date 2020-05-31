f96fa4ba01720e1aab8c83d287a8bb1195193390
jmarti <jmarti@theinit.com>
2020-05-31 17:00:46 +0200

    add simple keyboard with default layout

--- src/components/game/code-breaker/CodeBreaker.vue
+++ src/components/game/code-breaker/CodeBreaker.vue
@@ -1,10 +1,56 @@
 <template>
   <div>
-    hola
+    <input
+      :value="input"
+      class="input"
+      placeholder="Tap on the virtual keyboard to start"
+      @input="onInputChange"
+    >
+    <div class="keyboard">
+      <SimpleKeyboard :input="input" @onChange="onChange" @onKeyPress="onKeyPress" />
+    </div>
   </div>
 </template>
+<style scoped>
+  input {
+    margin-top: 5vh;
+    height: 5vh;
+    width: 20vw;
+    margin-bottom: 5vh;
+    text-align: center;
+    font-family: Consolas, Courier, serif;
+    text-transform: uppercase;
+    background-color: #111111;
+    border: solid 0.5vh rgb(212, 1, 47);
+    border-radius: 2vh;
+    color: white;
+  }
+  .keyboard {
+    width:30vw;
+    margin: auto;
+  }
+</style>
 <script>
+import SimpleKeyboard from './SimpleKeyboard';
+
 export default {
   name: 'CodeBreaker',
+  components: {
+    SimpleKeyboard,
+  },
+  data: () => ({
+    input: 'x'
+  }),
+  methods: {
+    onChange(input) {
+      console.log('input changed', input)
+    },
+    onKeyPress(button) {
+      console.log('button pressed', button)
+    },
+    onInputChange(input) {
+      this.input = input.target.value;
+    }
+  }
 }
 </script>
---
+++ src/components/game/code-breaker/SimpleKeyboard.vue
@@ -0,0 +1,66 @@
+<template>
+  <div :class="keyboardClass"></div>
+</template>
+
+<script>
+import Keyboard from "simple-keyboard";
+import "simple-keyboard/build/css/index.css";
+export default {
+  name: "SimpleKeyboard",
+  props: {
+    keyboardClass: {
+      default: "simple-keyboard",
+      type: String
+    },
+    input: {
+      type: String,
+      default: '',
+    }
+  },
+  data: () => ({
+    keyboard: null
+  }),
+  watch: {
+    input(input) {
+      this.keyboard.setInput(input);
+    }
+  },
+  mounted() {
+    this.keyboard = new Keyboard({
+      onChange: this.onChange,
+      onKeyPress: this.onKeyPress,
+      layout: {
+        'default': [
+          '1 2 3 4 5 6 7 8 9 0 {bksp}',
+          'Q W E R T Y U I O P',
+          'A S D F G H J K L {enter}',
+          'Z X C V B N M , .'
+        ],
+      },
+    });
+  },
+  methods: {
+    onChange(input) {
+      this.$emit("onChange", input);
+    },
+    onKeyPress(button) {
+      this.$emit("onKeyPress", button);
+      /**
+       * If you want to handle the shift and caps lock buttons
+       */
+      if (button === "{shift}" || button === "{lock}") this.handleShift();
+    },
+    handleShift() {
+      let currentLayout = this.keyboard.options.layoutName;
+      let shiftToggle = currentLayout === "default" ? "shift" : "default";
+      this.keyboard.setOptions({
+        layoutName: shiftToggle
+      });
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
